5553ab36af17e2aea53be05bc97cab8117a692c8
sagar <sagarbehera156@gmail.com>
2022-10-02 20:26:43 +0530

    Number to Word

---
+++ Number.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int a=1;a<=1000;a=a*10) {
+	//System.out.println(+a);
+	if(a==1)
+		System.out.println(a+":Unit");
+	if(a==10)
+		System.out.println(a+":Ten");
+	if(a==100)
+		System.out.println(a+":Hundred");
+	if(a==1000)
+		System.out.println(a+":One Thousand");
+
+}
+	}
+
+}
